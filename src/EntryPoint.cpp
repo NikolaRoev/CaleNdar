faac71fa1b07dc1a126ed5839c3fb5baf166897e
NikolaRoev <47421460+NikolaRoev@users.noreply.github.com>
2019-07-27 18:00:24 +0300

    Some setting up.

---
+++ src/EntryPoint.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "UI.h"
+
+
+
+int main() {
+
+
+
+
+	std::cin.get();
+}
---
+++ src/pch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//STL headers:
+#include <iostream>
+#include <vector>
+#include <array>
+#include <string>
+#include <functional>
+#include <utility>
+
+//------------
+
+//SFML:
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+//------------
